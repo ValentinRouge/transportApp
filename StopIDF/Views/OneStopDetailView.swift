686060c7fac5f7de9d2931db18cb4ef2a6d836f7
Valentin <rougevalentin@outlook.com>
2023-10-18 13:49:44 +0200

    Correct one stop view

---
+++ StopIDF/Views/OneStopDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  OneStopDetailView.swift
+//  StopIDF
+//
+//  Created by Valentin Rouge on 18/10/2023.
+//
+
+import SwiftUI
+
+struct OneStopDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OneStopDetailView()
+}
