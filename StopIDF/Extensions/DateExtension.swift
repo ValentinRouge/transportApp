e6fec02b67a87fc2c892dc780a844f0f5720ec3a
Valentin <rougevalentin@outlook.com>
2023-10-26 00:01:01 +0200

    Légère optimisation : on update la liste des que à chaque lancement d'app et pas à chaque apparition de la view

---
+++ StopIDF/Extensions/DateExtension.swift
@@ -0,0 +1,8 @@
+//
+//  DateExtension.swift
+//  StopIDF
+//
+//  Created by Valentin Rouge on 25/10/2023.
+//
+
+import Foundation
---
+++ StopIDF/LineContainer.swift
@@ -0,0 +1,8 @@
+//
+//  LineContainer.swift
+//  StopIDF
+//
+//  Created by Valentin Rouge on 25/10/2023.
+//
+
+import Foundation
